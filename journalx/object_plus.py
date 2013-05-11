d1fd080a64c9b82b7a6b9b887faac0ba59973872
Martin Abente Lahaye <tch@sugarlabs.org>
2013-05-11 15:46:52 -0400

    initial commit

---
+++ journalx/object_plus.py
@@ -0,0 +1,91 @@
+# Copyright (c) 2013 Martin Abente Lahaye. - tch@sugarlabs.org
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301 USA.
+
+import json
+import logging
+
+import error
+from object import Object
+from setting import Setting
+
+
+class ObjectPlus(Object):
+
+    def __init__(self, id=None):
+      Object.__init__(self)
+      self._id = id
+
+    def _get(self, url, params, completed_data, failed_data):
+        completed_id = self.connect('transfer-completed',
+                                    self._completed_cb, completed_data)
+        failed_id = self.connect('transfer-failed',
+                                 self._failed_cb, failed_data)
+        self.request('GET', Setting.get_url(url), params)
+        self.disconnect(completed_id)
+        self.disconnect(failed_id)
+
+    def _post(self, url, params, file, completed_data, failed_data):
+        completed_id = self.connect('transfer-completed',
+                                    self._completed_cb, completed_data)
+        failed_id = self.connect('transfer-failed',
+                                 self._failed_cb, failed_data)
+        self.request('POST', Setting.get_url(url), params, file)
+        self.disconnect(completed_id)
+        self.disconnect(failed_id)
+
+    def _delete(self, url, completed_data, failed_data):
+        completed_id = self.connect('transfer-completed',
+                                    self._completed_cb, completed_data)
+        failed_id = self.connect('transfer-failed',
+                                 self._failed_cb, failed_data)
+        self.request('DELETE', Setting.get_url(url))
+        self.disconnect(completed_id)
+        self.disconnect(failed_id)
+
+    def _completed_cb(self, object, data, signal):
+        try:
+            info = json.loads(data)
+            self._id_hook(info)
+            self.emit(signal, info)
+        except Exception, e:
+            logging.error('%s: _completed_cb crashed with %s',
+                          self.__class__.__name__, str(e))
+
+    def _failed_cb(self, object, message, signal):
+        self.emit(signal, message)
+
+    def _id_hook(self, info):
+        if 'id' in info.keys():
+            self._id = info['id']
+
+    def _file(self, field, path):
+        if not path:
+            return None
+
+        file = {}
+        file['field'] =  field
+        file['path'] = path
+
+        return file
+
+    def _check_is_not_created(self):
+        if self._id is not None:
+            raise error.AlreadyCreated('Already created')
+
+    def _check_is_created(self):
+        if self._id is None:
+            raise error.NotCreated('Not created')
---
+++ tests/test_entry.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2013 Martin Abente Lahaye. - tch@sugarlabs.org
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301 USA.
+
+import sys
+
+from gi.repository import GObject
+
+sys.path.append("..")
+from journalx.setting import Setting
+from journalx.entry import Entry
+
+
+Setting.set_url('http://localhost:8000')
+
+def __phase1_failed_cb(entry, info):
+    print '[FAILED] phase1: entries-posted-failed, with %s' % info
+    loop.quit()
+
+def __phase2_failed_cb(entry, info):
+    print '[FAILED] phase2: entries-downloaded-failed, with %s' % info
+    loop.quit()
+
+def __phase3_failed_cb(entry, info):
+    print '[FAILED] phase3: entries-updated-failed, with %s' % info
+    loop.quit()
+
+def __phase4_failed_cb(entry, info):
+    print '[FAILED] phase4: entries-deleted-failed, with %s' % info
+    loop.quit()
+
+
+def __phase1_cb(entry, info):
+    print '[OK] phase1: entries-posted, with: \n%s\n' % info
+    
+    entry.connect('entry-downloaded', __phase2_cb)
+    entry.connect('entry-downloaded-failed', __phase2_failed_cb)
+    entry.get()
+
+def __phase2_cb(entry, info):
+    print '[OK] phase2: entry-downloaded, with: \n%s\n' % info
+
+    entry.connect('entry-updated', __phase3_cb)
+    entry.connect('entry-updated-failed', __phase3_failed_cb)
+    entry.update('new-title', 'new-description', 'screenshot.png')
+
+def __phase3_cb(entry, info):
+    print '[OK] phase3: entry-updated, with: \n%s\n' % info
+
+    entry.connect('entry-deleted', __phase4_cb)
+    entry.connect('entry-deleted-failed', __phase4_failed_cb)
+    entry.delete()
+
+def __phase4_cb(entry, info):
+    print '[OK] phase4: entries-deleted, with: \n%s\n' % info
+    loop.quit()
+
+entry = Entry()
+entry.connect('entry-posted', __phase1_cb)
+entry.connect('entry-posted-failed', __phase1_failed_cb)
+entry.post('title', 'description', 'screenshot.png')
+
+loop = GObject.MainLoop()
+loop.run()
